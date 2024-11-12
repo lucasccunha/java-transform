300990997b7a2fd2ed3b612b300a05354e2fcf08
Lucas Cunha <lucas71cunha@gmail.com>
2024-11-11 23:15:08 -0300

    Add Test

--- src/test/java/br/com/fiap/refl/TransformatorTest.java
+++ src/test/java/br/com/fiap/refl/TransformatorTest.java
@@ -1,5 +1,6 @@
 package br.com.fiap.refl;
 
+import br.com.fiap.Endereco;
 import br.com.fiap.Pessoa;
 import br.com.fiap.PessoaDTO;
 import org.junit.jupiter.api.Assertions;
@@ -10,6 +11,7 @@ import java.lang.reflect.InvocationTargetException;
 public class TransformatorTest {
 
     Pessoa pessoa = new Pessoa(1, "Lucas", "1234");
+    Endereco endereco = new Endereco("Rua Teste", 1);
 
     @Test
     public void shouldTransform() throws ClassNotFoundException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
@@ -19,5 +21,23 @@ public class TransformatorTest {
         Assertions.assertInstanceOf(PessoaDTO.class, pessoaDTO);
         Assertions.assertEquals(pessoa.getNome(), pessoaDTO.getNome());
         Assertions.assertEquals(pessoa.getCpf(), pessoaDTO.getCpf());
+    }
+
+    @Test
+    public void sholdNotTransform() {
+        Assertions.assertThrows(ClassNotFoundException.class, () -> {
+            Transformator transformator = new Transformator();
+            transformator.transform(endereco);
+        });
+    }
+
+    @Test
+    public void shouldTransformWhenSomeFieldIsNull() throws ClassNotFoundException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
+        Pessoa pessoaSemCPF = new Pessoa("Lucas");
+        Transformator transformator = new Transformator();
+        PessoaDTO pessoaDTOSemCPF = transformator.transform(pessoaSemCPF);
+
+        Assertions.assertEquals(pessoa.getNome(), pessoaDTOSemCPF.getNome());
+        Assertions.assertNull(pessoaDTOSemCPF.getCpf());
     }
 }
